d62c339e7960779d2dea0c4cd929423ff0c92f52
Olga-Pro <oip_@inbox.ru>
2024-05-17 09:53:40 +0300

    castorama

--- castoramasvet/castoramasvet/spiders/castoramasvet.py
+++ castoramasvet/castoramasvet/spiders/castoramasvet.py
@@ -1,11 +1,11 @@
 import scrapy
 
 
-class DivansvetSpider(scrapy.Spider):
-    name = "divansvet"
-
+class CastoramasvetSpider(scrapy.Spider):
+    name = "castoramasvet"
     allowed_domains = ["https://www.castorama.ru"]
     start_urls = ["https://www.castorama.ru/lighting/interior-lighting/"]
+
 
     def parse(self, response):
         svet_page = response.css('li.product-card')
@@ -15,3 +15,4 @@ class DivansvetSpider(scrapy.Spider):
                 'price': svet.css('div.pY3d2 span::text')[3].get(),  # ценв в четвертом элементе
                 'link': svet.css('a.product-card__name').attrib['href']
             }
+
